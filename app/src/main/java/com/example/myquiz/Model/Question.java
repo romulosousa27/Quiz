a65730ee2e1210ca1aeb3ac4059a350d4cceddaf
Romulo Sousa <romulosousa27@hotmail.com>
2019-03-01 00:21:41 -0300

    Classe de Questões

---
+++ app/src/main/java/com/example/myquiz/Model/Question.java
@@ -0,0 +1,45 @@
+package com.example.myquiz.Model;
+
+
+public class Question {
+
+    private String text;
+    private Double answerCorrect;
+    private Double answerIncorrect;
+
+    /**
+     *
+     * @param text
+     * @param answerCorrect
+     * @param answerIncorrect
+     */
+    public Question(String text, double answerCorrect, double answerIncorrect){
+        this.text = text;
+        this.answerCorrect = answerCorrect;
+        this.answerIncorrect = answerIncorrect;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getText(){
+        return text;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getAnserCorrect(){
+        return answerCorrect;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Double getAnswerIncorrect() {
+        return answerIncorrect;
+    }
+}
